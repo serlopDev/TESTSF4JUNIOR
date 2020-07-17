0b41e0e203ba4f9d5161ea947c3547e8b92d34cf
Sergio López <sergiolopez@MacBook-Pro.local>
2020-07-17 09:56:21 +0200

    Creada la plantilla de edición de usuarios y método de búsqueda por id

--- src/Controller/UsersListController.php
+++ src/Controller/UsersListController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersListController extends AbstractController
 {
+    // Lista de usuarios
+
+
     /**
-     * @Route("/users/list", name="users_list")
+     * @Route("/usuarios", name="users_list")
      */
-    public function index()
+    public function index(EntityManagerInterface $em)
     {
+        $usersList = $em->getRepository(User::class)->findAll();
         return $this->render('users_list/users_list.html.twig', [
             'controller_name' => 'UsersListController',
+            'users'=> $usersList,
         ]);
+    }
+
+    // Edición de usuario
+
+    /**
+     * @Route("/usuarios/{id}", name="edit_user")
+     */
+    public function editUser($id){
+        $em = $this->getDoctrine()->getManager();
+        $user = $em -> getRepository(User::class)-> find($id);
+
+        return $this->render('users_list/editUser.html.twig', ['user'=> $user]);
     }
 }
